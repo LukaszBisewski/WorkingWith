e1dab7d54a3ab791f4ae9b06346b7b9fb0f48159
LukaszBisewski <lukaszbis94@gmail.com>
2020-03-24 14:07:53 +0100

    Dodaj pliki projektów.

---
+++ WorkingWith/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
