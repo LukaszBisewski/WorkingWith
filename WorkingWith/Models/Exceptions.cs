5ed32f1a966b6037cdc8a2901859939c419a5cf5
LukaszBisewski <lukaszbis94@gmail.com>
2020-03-29 12:52:44 +0200

    Generic, exception, var
    How to deal with errors occurring as exceptions and how useful the generics.
    Use the var keyword in order to reduce an amount of unnecessary code and declare anonymous objects.

---
+++ WorkingWith/Models/Exceptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkingWith.Models
+{
+    class Exceptions
+    {
+        public void Test()
+        {
+            try
+            {
+                User user = new User("user1@email.com", "Secret");
+                user = null;
+                throw new ArgumentNullException(nameof(user));
+
+                //Sign up user...
+                //Email in use
+                throw new Exception("Adres Email w użyciu.");
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine($"Null error: {exception}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"An error: {exception}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally here!");
+            }
+
+            Console.WriteLine("OK");
+        }
+    }
+}
--- WorkingWith/Program.cs
+++ WorkingWith/Program.cs
@@ -7,14 +7,29 @@ namespace WorkingWith.Models
     {
         public static void Main(string[] args)
         {
-            Race race = new Race();
-            race.Begin();
+            //Race race = new Race();
+            //race.Begin();
 
-            Shop shop = new Shop();
-            shop.CompleteOrder();
-            shop.CompletFakeOrder();
-            Console.ReadLine();
+            //Shop shop = new Shop();
+            //shop.CompleteOrder();
+            //shop.CompletFakeOrder();
+            //Console.ReadLine();
+
+            var user = new User("user@email.com", "secret");
+
+            var anotherUser = new
+            {
+                Id = 1,
+                Name = "user",
+                Address = new
+                {
+                    Street = "Krakowska 1",
+                    City = "Kraków"
+                }
+            };
+
         }
     }
 }
+
 
